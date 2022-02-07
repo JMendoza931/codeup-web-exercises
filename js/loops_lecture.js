1f8ed7d83c5b787ce56062b4917ee3639ddf37ae
Jose A. Mendoza <jose.a.mendoza931@gmail.com>
2022-02-07 12:18:30 -0600

    adding conditionals hmtl and loops lecture js and html files

---
+++ js/loops_lecture.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// var slices = 4
+// while (slices > 0){
+//     console.log("i'll have a slice!");
+//     slices = slices -1;
+//     console.log("now there's " + slices + "left");
+// }
+// console.log("no more pizza")
+
+//
+// var counter = 0;
+// while (counter <= 100){
+//     console.log(counter);
+//     counter++;
+// }
+// var counter = 100;
+// while(counter >= 0){
+//     console.log(counter)
+//     counter--;
+// }
+
+// var counter = 1;
+// var number = 5;
+// while (number < 200){
+//     number = number * 2;
+//     console.log(number);
+// }
+
+var counter = 1;
+var number = 2
+while(number <= 70000){
+    counter = counter +1
+    console.log(number);
+    number = number * 2;
+}
